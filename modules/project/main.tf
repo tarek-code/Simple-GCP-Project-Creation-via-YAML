facb4d785b661bcfb2ac0926d891af6be7dc360c
tarek-code <tarekadel314@gmail.com>
2025-09-28 14:55:23 +0300

    Add deletion policy to Google Cloud project configuration
    - Introduced a deletion policy set to "DELETE" in the main Terraform configuration for the Google project resource to manage resource lifecycle more effectively.

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -4,6 +4,7 @@ resource "google_project" "project" {
   org_id          = var.organization_id != null ? var.organization_id : null
   billing_account = var.billing_account
   labels          = var.labels
+  deletion_policy = "DELETE"
 }
 
 # Enable APIs
